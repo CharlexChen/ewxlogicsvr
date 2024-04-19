eff4027fdd34e466e9618ec7e9c1d590f7148178
CharlesChanV <charleschan2015@hotmail.com>
2024-04-19 09:47:14 +0800

    feat(web): import arco and layout

--- web/App.vue
+++ web/App.vue
@@ -1,3 +1,10 @@
 <template>
-    <router-view></router-view>
+    <a-config-provider>
+        <router-view></router-view>
+    </a-config-provider>
 </template>
+<style lang="less">
+.app {
+    height: 200px;
+}
+</style>
--- web/main.ts
+++ web/main.ts
@@ -1,4 +1,7 @@
 import { createVueApp } from "./create";
-
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+import '@/assets/style/global.less';
 const { app, router: _router } = createVueApp();
+app.use(ArcoVue);
 app.mount('#app');
